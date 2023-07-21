87dfe0298e4e84a17095c785c3393ab191674bb8
IAMKOTARO <saitou.kotarou898@mail.kyutech.jp>
2023-07-21 22:00:24 +0900

    ユニットテストを通過

--- app/utils/fibonacci.py
+++ app/utils/fibonacci.py
@@ -1,4 +1,10 @@
 def fibonacci(n: int):
-  if n <= 1:
-    return n
+  if type(n) != int:
+    raise ValueError("Invalid input")
+  elif n < 0:
+    raise ValueError("Invalid input")
+  elif n == 0:
+    return 0
+  elif n <= 2:
+    return 1
   return fibonacci(n - 1) + fibonacci(n - 2)
