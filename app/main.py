8f3f0fce599d8e8e5c2ed8559704b2dc8cc84488
IAMKOTARO <saitou.kotarou898@mail.kyutech.jp>
2023-07-21 22:07:11 +0900

    エンドポイントを実装

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
+from utils import fibonacci
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
   
+@app.get("/fib")
+def read_fib(n: int):
+  # フィボナッチ数列を返す
+  try:
+    result = fibonacci(n)
+  except ValueError as e:
+    return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"status": status.HTTP_400_BAD_REQUEST, "message": "Bad Request"})
+  except Exception as e:
+    return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={"status": status.HTTP_500_INTERNAL_SERVER_ERROR, "message": "Internal Server Error"})
+  
+  return JSONResponse(status_code=status.HTTP_200_OK, content={"result": result})
