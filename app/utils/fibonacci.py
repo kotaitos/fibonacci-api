5f47da80a8e540b2783d948a4a44fd84cdc30fdc
IAMKOTARO <saitou.kotarou898@mail.kyutech.jp>
2023-07-27 15:06:10 +0900

    コードのフォーマット

--- app/utils/fibonacci.py
+++ app/utils/fibonacci.py
@@ -21,8 +21,8 @@ def fibonacci(n: int):
   
   fib_sequence = [0, 1]
   for i in range(2, n + 1):
-      fib_num = fib_sequence[i - 1] + fib_sequence[i - 2]
-      fib_sequence.append(fib_num)
+    fib_num = fib_sequence[i - 1] + fib_sequence[i - 2]
+    fib_sequence.append(fib_num)
   
   if r:
     r.set(n, fib_sequence[n])
